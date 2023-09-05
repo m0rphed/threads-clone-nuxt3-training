d20d1f24f028ef897bdfbc9d619adea8fd68a56c
m0rphed <35064145+m0rphed@users.noreply.github.com>
2023-09-05 12:20:07 +0300

    upd nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,9 +4,14 @@ export default defineNuxtConfig({
   modules: [
     'nuxt-icon',
     '@nuxtjs/tailwindcss',
-    '@nuxtjs/supabase',
+    //'@nuxtjs/supabase',
     '@pinia/nuxt',
     '@vite-pwa/nuxt',
   ],
+  runtimeConfig: {
+    public: {
+      bucketUrl: process.env.BUCKET_URL,
+    },
+  },
   devtools: { enabled: true },
 });
